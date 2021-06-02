c91fe62c57ec6a8a1239b9f509e87e02f01b85d1
unknown <onifaie@gmail.com>
2021-06-02 11:40:49 +0300

    add customer model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from . models import obeid
+from . models import obeid,customer
 # Register your models here.
 admin.site.register(obeid)
+admin.site.register(customer)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class obeid(models.Model):
     pass
+class customer(models.Model):
+    name_customer=models.CharField(max_length=100,null=False)
+    date_created=models.DateTimeField(auto_now=datetime.now)
+    
